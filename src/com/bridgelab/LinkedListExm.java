e54dfdc9d517c8703be7ef91bc99db0fe090bb70
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-21 18:18:23 +0530

    UC4-Insert 30 between 56 and 70

--- src/com/bridgelab/LinkedListExm.java
+++ src/com/bridgelab/LinkedListExm.java
@@ -1,6 +1,6 @@
 package com.bridgelab;
 
-//UC3 - Ability to create LinkedList by appending 30 and 70 to 56
+//UC4 - Ability to insert 30 between 56 and 70
 
 import java.util.LinkedList;
 
@@ -8,8 +8,8 @@ public class LinkedListExm {
     public static void main(String[] args) {
         System.out.println("Welcome to the LinkedList Problem...");
         LinkedList<String> list = new LinkedList<>();
+        list.add("56");
         list.add("30");
         list.add("70");
-        list.add("56");
     }
 }
